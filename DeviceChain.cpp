d3227d976d78aae024cc1147a1d7e938a01c4cbf
HumaSOFT <humasoft@ing.uc3m.es>
2018-04-18 14:20:48 +0200

    modificado:    DeviceChain.cpp 	modificado:    DeviceChain.h

--- DeviceChain.cpp
+++ DeviceChain.cpp
@@ -12,6 +12,20 @@ DeviceChain::DeviceChain(string actualPort)
         Joints[i]=CiA402Device(i, &Ports[i]);
     }
 }
+
+DeviceChain::DeviceChain(string actualPort, vector<long> ids)
+{
+    Ports.resize(6);
+    Joints.resize(6);
+
+    for(int i=0; i<6; i++){
+        Ports[i]=SocketCanPort(actualPort);
+    }
+    for(int i=0; i<6; i++){
+        Joints[i]=CiA402Device(ids[i], &Ports[i]);
+    }
+
+}
 long DeviceChain::SetPositions(vector<double> jointAngles){
 
     for(int i=0; i<6; i++){
--- DeviceChain.h
+++ DeviceChain.h
@@ -8,6 +8,8 @@ class DeviceChain
 {
 public:
     DeviceChain(string actualPort);
+    DeviceChain(string actualPort, vector<long> ids);
+
     long SetPositions(vector<double> jointAngles);
     long SetupPositionMode(const uint32_t velocity,const uint32_t acceleration);
 private:
