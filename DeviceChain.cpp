cec2d6545ffb37d01735a98972f08e2869ae9b0f
AlvaroMartinezR <100317213@alumnos.uc3m.es>
2018-04-18 13:34:37 +0200

    new file:   DeviceChain.cpp 	new file:   DeviceChain.h

---
+++ DeviceChain.cpp
@@ -0,0 +1,28 @@
+#include "DeviceChain.h"
+
+DeviceChain::DeviceChain(string actualPort)
+{
+    Ports.resize(6);
+    Joints.resize(6);
+
+    for(int i=0; i<6; i++){
+        Ports[i]=SocketCanPort(actualPort);
+    }
+    for(int i=0; i<6; i++){
+        Joints[i]=CiA402Device(i, &Ports[i]);
+    }
+}
+long DeviceChain::SetPositions(vector<double> jointAngles){
+
+    for(int i=0; i<6; i++){
+        Joints[i].SetPosition(jointAngles[i]);
+    }
+    return 0;
+}
+
+long DeviceChain::SetupPositionMode(const uint32_t velocity,const uint32_t acceleration){
+    for(int i=0; i<6; i++){
+        Joints[i].SetupPositionMode(velocity, acceleration);
+    }
+    return 0;
+}
---
+++ DeviceChain.h
@@ -0,0 +1,18 @@
+#ifndef DEVICECHAIN_H
+#define DEVICECHAIN_H
+
+#include "Cia402device.h"
+#include "SocketCanPort.h"
+
+class DeviceChain
+{
+public:
+    DeviceChain(string actualPort);
+    long SetPositions(vector<double> jointAngles);
+    long SetupPositionMode(const uint32_t velocity,const uint32_t acceleration);
+private:
+    std::vector <CiA402Device> Joints;
+    std::vector <SocketCanPort> Ports;
+};
+
+#endif // DEVICECHAIN_H
